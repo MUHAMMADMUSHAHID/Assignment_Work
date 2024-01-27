4442aed118d98fde8afefdbac8de4d2f32bb7f4b
Mushahid7 <shahidmad669@gmail.com>
2024-01-27 10:28:51 +0530

    Assignment Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RuleForm from './RuleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <RuleForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RuleForm from './RuleForm';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RuleForm />
+  </React.StrictMode>
+);
